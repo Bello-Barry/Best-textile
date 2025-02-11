b0f206a2b8ee1f19e66bc35b7da3b4f39172d200
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2025-02-11 14:37:04 +0100

    Update ProductCard.tsx

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -65,16 +65,16 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
     });
 
     toast.success(
-      <div className="flex items-center">
-        <Check className="mr-2 h-5 w-5 text-green-500" />
-        {quantity} mètre{quantity > 1 ? "s" : ""} de "{product.name}" ajouté
-        {quantity > 1 ? "s" : ""} au panier
-      </div>,
-      {
-        icon: false,
-        progressClassName: "bg-green-500",
-      }
-    );
+  <div className="flex items-center">
+    <Check className="mr-2 h-5 w-5 text-green-500" />
+    {quantity} mètre{quantity > 1 ? "s" : ""} de &quot;{product.name}&quot; ajouté
+    {quantity > 1 ? "s" : ""} au panier
+  </div>,
+  {
+    icon: false,
+    progressClassName: "bg-green-500",
+  }
+);
   };
 
   return (
