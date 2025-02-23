5409b3846ce7e44369cef48bb460a14747169c61
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2025-02-23 23:09:44 +0100

    Update cartStore.ts

--- store/cartStore.ts
+++ store/cartStore.ts
@@ -1,12 +1,13 @@
-//store/cartstore
 import { create } from "zustand";
 
-interface CartItem {
+export interface CartItem {
   id: string;
   name: string;
   price: number;
   quantity: number;
   images: string[];
+  unit: "mètre" | "rouleau";
+  fabricType: string;
 }
 
 interface CartState {
@@ -20,7 +21,19 @@ interface CartState {
 export const useCartStore = create<CartState>((set) => ({
   cartItems: [],
   addToCart: (item) =>
-    set((state) => ({ cartItems: [...state.cartItems, item] })),
+    set((state) => {
+      const existingItem = state.cartItems.find((i) => i.id === item.id);
+      if (existingItem) {
+        return {
+          cartItems: state.cartItems.map((i) =>
+            i.id === item.id 
+              ? { ...i, quantity: i.quantity + item.quantity }
+              : i
+          )
+        };
+      }
+      return { cartItems: [...state.cartItems, item] };
+    }),
   removeFromCart: (id) =>
     set((state) => ({
       cartItems: state.cartItems.filter((item) => item.id !== id),
