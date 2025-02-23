9b07dcdbbee754b52ba520fe88c63e748e0fb30a
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2025-02-23 06:55:31 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,16 +5,22 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "*.supabase.co", // Pour les images stockées sur Supabase
+        hostname: "*.supabase.co",
         port: "",
-        pathname: "/**",
-      },
+        pathname: "/**"
+      }
     ],
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 604800, // 1 semaine
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384]
   },
   experimental: {
-    esmExternals: true,
+    esmExternals: true
   },
   reactStrictMode: true,
+  // Optionnel : Activation de la compression Brotli
+  compress: true
 };
 
 export default nextConfig;
