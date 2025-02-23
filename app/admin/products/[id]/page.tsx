d34432c36d939e256f11624ee38302a144b0b770
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2025-02-23 21:56:46 +0100

    Update page.tsx

--- app/admin/products/[id]/page.tsx
+++ app/admin/products/[id]/page.tsx
@@ -8,7 +8,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { toast } from "react-toastify";
-import ProductGallery from "@/components/ProductGallery";
+import ProductGallery from "@/components/ui/ProductGallery";
 
 // Schéma de validation étendu avec les nouveaux champs
 const schema = z.object({
