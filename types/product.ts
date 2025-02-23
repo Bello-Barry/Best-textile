5d769550a58949e3c8a71bca37220eaa4f035575
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2025-02-23 22:22:12 +0100

    Update product.ts

--- types/product.ts
+++ types/product.ts
@@ -1,9 +1,12 @@
+// types/product.ts
 export interface Product {
   id: string;
   name: string;
   description: string;
   price: number;
+  stock: number;
+  fabricType: string;
+  fabricSubtype?: string;
+  unit: "mètre" | "rouleau";
   images: string[];
-  type: "soie" | "bazin" | "autre";
-  subtype?: "soie fleuri" | "soie plissé" | "bazin riche" | "getzner";
 }
