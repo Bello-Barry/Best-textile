2ad4c9ac134985432b0fe8fd97391db45eda93f7
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2025-03-07 06:21:40 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,17 @@
 // next.config.mjs
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "*.supabase.co", // Ajout du wildcard pour tous les sous-domaines
+        hostname: "*.supabase.co",
         pathname: "/storage/v1/object/public/images/**",
       },
     ],
   },
   experimental: {
-    esmExternals: true,
+    esmExternals: "loose", // Utiliser "loose" au lieu de true pour plus de compatibilité
   },
   reactStrictMode: true,
   compress: true,
