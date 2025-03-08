c90c95a6ffb06488cbeca591a0c0f760c780154f
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2025-03-08 17:13:54 +0100

    Update CategoryNav.tsx

--- components/CategoryNav.tsx
+++ components/CategoryNav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import * as React from "react";
+import { useMemo } from "react"; // Ajout de useMemo
 import { cn } from "@/lib/utils";
 import { Shirt, Sofa, Box } from "lucide-react";
 import { Button } from "@/components/ui/button";
